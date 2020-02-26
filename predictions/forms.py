0c7fded3c8a5593307718a1c5ddca97c518f9ba8
Sak1361 <0601aiueo@gmail.com>
2020-02-26 19:25:24 +0900

    resetting

--- predictions/forms.py
+++ predictions/forms.py
@@ -4,9 +4,34 @@ from django.contrib.auth import get_user_model
 from django import forms
 
 
+class FileInputWithPreview(forms.ClearableFileInput):
+    """プレビュー表示されるinput type=file"""
+    template_name = 'predictions/widgets/file_input_with_preview.html'
+
+    class Media:
+        js = ['predictions/js/preview.js']
+
+    def __init__(self, attrs=None, include_preview=False):
+        super().__init__(attrs)
+        if 'class' in self.attrs:
+            self.attrs['class'] += 'preview-marker'
+        else:
+            self.attrs['class'] = 'preview-marker'
+        self.include_preview = include_preview
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['widget'].update({
+            'include_preview': self.include_preview,
+        })
+        return context
+
+
 class Photo_form(forms.Form):
-    image = forms.ImageField(widget=forms.FileInput(
-        attrs={'class': 'custom-file-input'}))
+    image = forms.ImageField(widget=FileInputWithPreview())
+
+    # image = forms.ImageField(widget=forms.FileInput(
+    #    attrs={'class': 'custom-file-input'}))
 
 
 User = get_user_model()
